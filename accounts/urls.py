f45a648cdae43c8fbef89d2d284a64e2949c9f8c
kakikukeko70 <tsusu3292@gmail.com>
2022-04-21 18:38:39 +0900

    signup用urlを追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import Accounts
+from .views import Accounts, SignUpView, ActivateView 
 
 app_name = 'accounts'
 urlpatterns = [
     path('account/', Accounts.as_view(), name='accounts'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('activate/', ActivateView.as_view(), name='activate'),
 ]
