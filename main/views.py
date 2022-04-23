41557420f957ea818646acc2d19a9660661d8e0b
kakikukeko70 <tsusu3292@gmail.com>
2022-04-23 17:25:44 +0900

    memo関連のビューを追加

--- main/views.py
+++ main/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from accounts.forms import MemoForm
+from django.urls import reverse
+from django.http import HttpResponseRedirect 
+from accounts.models import User
+from django.shortcuts import get_object_or_404
 
 class Index(TemplateView):
     template_name = 'main/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['memo_form'] = MemoForm() 
+        return context
+
 class TodoList(TemplateView):
     template_name = 'main/todo.html'
+    
+def upload_memo(request):
+    form = MemoForm(request.POST)
+    is_valid = form.is_valid()
+    if is_valid:
+        content = form.cleaned_data['content']
+        user = get_object_or_404(User, pk=request.user.id)
+        user.memo = content
+        user.save()
+        return HttpResponseRedirect(reverse('main:index'))
