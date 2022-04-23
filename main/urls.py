65e4755ff33125b58fd7cc8997ca24bb4e94ea95
kakikukeko70 <tsusu3292@gmail.com>
2022-04-23 17:24:24 +0900

    Indexビューをログイン必須にした memoをupdateするためurlを追加

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
-from xml.etree.ElementInclude import include
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
-from .views import Index, TodoList
+from .views import Index, TodoList, upload_memo
 
 
 app_name='main'
 urlpatterns = [
-    path('', Index.as_view(), name='index'),
+    path('', login_required(Index.as_view()), name='index'),
     path('todo/', TodoList.as_view(), name='todo'),
+    path('upload_memo>/', upload_memo, name='upload_memo'),
 ]
