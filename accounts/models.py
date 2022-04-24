3dbfa71a3f6efd275f491da8a6cc3746c308a585
kakikukeko70 <tsusu3292@gmail.com>
2022-04-24 22:56:15 +0900

    Folder, Bookmarkモデルを追加

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,19 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField('email address', unique=True)
     memo = models.TextField(default='')
+    
+class Folder(models.Model):
+    name = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class Bookmark(models.Model):
+    name = models.CharField(max_length=30)
+    url = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
