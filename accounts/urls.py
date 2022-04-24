ecc9d235e40224bba13ff3dbccec04f70a969b54
kakikukeko70 <tsusu3292@gmail.com>
2022-04-24 22:57:10 +0900

    viewクラスの名前を変更

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from .views import Accounts, SignUpView, ActivateView, Verify, change_username 
+from .views import AccountView, SignUpView, ActivateView, VerifyView, change_username 
 
 app_name = 'accounts'
 urlpatterns = [
-    path('account/', Accounts.as_view(), name='accounts'),
+    path('account/', AccountView.as_view(), name='accounts'),
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('email_sent/', Verify.as_view(), name='verify'),
+    path('email_sent/', VerifyView.as_view(), name='verify'),
     path('activate/<uidb64>/<token>/', ActivateView.as_view(), name='activate'),
     path('change_username', change_username, name='change_username')
 ]
