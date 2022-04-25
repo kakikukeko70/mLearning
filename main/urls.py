3313609305a6898cb811d1e2f7afdd9247936aff
kakikukeko70 <tsusu3292@gmail.com>
2022-04-25 22:22:23 +0900

    functionviewをクラス内においたのを編集 bookmark編集関連のurlを追加

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include
-from .views import IndexView, TodoListView, BookmarksView, EditBookmark, FolderdetailView, upload_memo, create_folder
+from .views import IndexView, TodoListView, FolderView, EditBookmark, FolderdetailView
 
 app_name='main'
 urlpatterns = [
     path('', login_required(IndexView.as_view()), name='index'),
     path('todo/', TodoListView.as_view(), name='todo'),
-    path('bookmarks/', BookmarksView.as_view(), name='bookmarks'),
+    path('bookmark_folders/', FolderView.as_view(), name='folders'),
     path('edit_bookmark/<int:pk>/', EditBookmark.as_view(), name='editbookmark'),
     path('folderdetail/<int:pk>', FolderdetailView.as_view(), name='folderdetail'),
-    path('upload_memo/', upload_memo, name='upload_memo'),
-    path('create_folder/', create_folder, name='create_folder'),
+    path('upload_memo/', IndexView.upload_memo, name='upload_memo'),
+    path('create_folder/', FolderView.create_folder, name='create_folder'),
+    path('change_bookmark_name/<int:id>/', EditBookmark.change_bookmark_name, name='change_bookmark_name'),
+    path('change_folder/<int:id>/', EditBookmark.change_folder, name='change_folder'),
+    path('delete_bookmark/<int:id>', EditBookmark.delete_bookmark, name='delete_bookmark'),
 ]
