06a8044778028a5c0ec5af1693e1461d15cc63c7
kakikukeko70 <tsusu3292@gmail.com>
2022-04-27 21:49:00 +0900

    Todoモデルを追加

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Folder, Bookmark
+from .models import User, Folder, Bookmark, Todo
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Folder)
 admin.site.register(Bookmark)
+admin.site.register(Todo)
