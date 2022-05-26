6a5353c261e67f6ac8fd3a92dc0f7b743718ff32
kakikukeko70 <tsusu3292@gmail.com>
2022-05-26 16:09:09 +0900

    Memo modelを追加

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Folder, Bookmark, Todo
+from .models import User, Memo, Folder, Bookmark, Todo
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Memo)
 admin.site.register(Folder)
 admin.site.register(Bookmark)
 admin.site.register(Todo)
