80d7cd42cc046da0a19254f397f2f2b866075f4c
kakikukeko70 <tsusu3292@gmail.com>
2022-05-27 17:40:00 +0900

    test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from datetime import date
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from accounts.models import Memo
+
+User = get_user_model()
+
+class IndexTests(TestCase):
+
+    @classmethod 
+    def setUpTestData(cls):
+        user = User.objects.create(username='test', email='test@invalid.com')
+        user.set_password('testuserpass')
+        user.save()
+        memo = Memo.objects.create(memo='', user=user)
+        memo.save()
+
+    def test_add_task(self):
+        self.client.login(username='test', password='testuserpass')
+        today = date.today()
+        
+        data = {'text': ['統計学'], 'deadline': [today]}
+        response = self.client.post('/add_todo/', data=data, follow=True)
+        self.assertEqual(response.request['PATH_INFO'], '/')
+        self.assertEqual(response.status_code, 200)
+
+        data = {'text': ['統計学']}
+        response = self.client.post('/add_todo/', data=data, follow=True)
+        self.assertEqual(response.request['PATH_INFO'], '/error/')
+
+        # for item in dir(response):
+        #     print(item)
+    
+    def update_todo(self):
+        self.client.login(username='test', password='testuserpass')
+        user = User.objects.get(username='test')
+
