7db340eb08793eeaa7e8df2b464600fddd7b8a3f
kakikukeko70 <tsusu3292@gmail.com>
2022-05-27 17:48:24 +0900

    ChangedTemplatePasswordChangeViewとChangedTemplatePasswordResetViewを追加したことによるpathの追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import AccountView, CangeUserNameView, UserDeleteView, SignUpView, ActivateView, VerifyView
+from .views import AccountView, CangeUserNameView, UserDeleteView, SignUpView, ActivateView, VerifyView, ChangedTemplatePasswordChangeView, ChangedTemplatePasswordResetView
 
 app_name = 'accounts'
 urlpatterns = [
@@ -10,4 +10,6 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('email_sent/', VerifyView.as_view(), name='verify'),
     path('activate/<uidb64>/<token>/', ActivateView.as_view(), name='activate'),
+    path('password_change/', ChangedTemplatePasswordChangeView.as_view(), name='password_change'),
+    path('password_reset/', ChangedTemplatePasswordResetView.as_view(), name='password_reset'),
 ]
