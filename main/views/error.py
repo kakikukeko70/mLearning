e507ae3c8ba35eecbf8ab90f52719f7e4c4e9aef
kakikukeko70 <tsusu3292@gmail.com>
2022-06-06 23:45:40 +0900

    invalid_urlのviewを追加

--- main/views/error.py
+++ main/views/error.py
@@ -2,3 +2,6 @@ from django.views.generic.base import TemplateView
 
 class ErrorView(TemplateView):
     template_name = 'error/error.html'
+
+class InvalidUrlView(TemplateView):
+    template_name = 'error/invalid_url.html'
